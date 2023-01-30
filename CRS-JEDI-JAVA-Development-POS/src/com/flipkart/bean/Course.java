7e28fa03b5964419abee7391ba63118e65179427
Shivanshu <shivanshuyadav37@gmail.com>
2023-01-30 16:29:29 +0530

    admin couse

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Course.java
@@ -0,0 +1,32 @@
+package com.flipkart.bean;
+
+public class Course {
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getInstructorId() {
+        return instructorId;
+    }
+
+    public void setInstructorId(String instructorId) {
+        this.instructorId = instructorId;
+    }
+
+    private String courseCode;
+    private String courseName;
+    private String instructorId;
+    private int seats = 10;
+}
