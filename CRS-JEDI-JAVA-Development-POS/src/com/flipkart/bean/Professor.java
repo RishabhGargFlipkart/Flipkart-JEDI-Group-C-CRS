63fe2f65813cc5827cf04331bc66e0f45499e2fa
RishabhGargFlipkart <123539417+RishabhGargFlipkart@users.noreply.github.com>
2023-01-30 16:31:37 +0530

    professor class added

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Professor.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.flipkart.bean;
+
+/**
+ * @author rishabh.garg
+ *
+ */
+public class Professor extends User {
+	private String department;
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+}
