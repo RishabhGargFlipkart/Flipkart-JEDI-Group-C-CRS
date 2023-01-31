32133b7cec6d2c638e523d2723a6a8e2135d96b0
akhilmac8 <akhil.macherla@flipkart.com>
2023-01-31 11:11:29 +0530

    Profesor Services

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/service/ProfessorServiceOperation.java
@@ -0,0 +1,17 @@
+package com.flipkart.service;
+import com.flipkart.bean.Course;
+import com.flipkart.bean.Student;
+import java.util.List;
+public class ProfessorServiceOperation {
+    public String addGrade(int profId, int studentId, String courseCode, int semester){
+        return null;
+    }
+
+    public List<Student> viewEnrolledStudents(int profId, String courseCode){
+        return null;
+    }
+
+    public List<Course> getCourses(int profId){
+        return null;
+    }
+}
