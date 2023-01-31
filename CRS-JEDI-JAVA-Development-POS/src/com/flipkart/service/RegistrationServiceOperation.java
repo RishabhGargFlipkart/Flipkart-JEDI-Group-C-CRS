433deb8beaf4864f2d63605fd835f6402b331ad9
Manan Gulati <manan.gulati@BLRETV-Y9X4WDNV.local>
2023-01-31 11:19:17 +0530

    Added Registration Service Operation

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/service/RegistrationServiceOperation.java
@@ -0,0 +1,36 @@
+package com.flipkart.service;
+
+import com.flipkart.bean.Course;
+import com.flipkart.bean.StudentGrade;
+
+import java.util.List;
+
+public class RegistrationServiceOperation {
+
+    private RegistrationServiceOperation() {
+    }
+
+    public boolean addCourse(){
+        return false;
+    }
+
+    public boolean dropCourse(){
+        return false;
+    }
+
+    public List<StudentGrade> viewGradeCard(){
+        return null;
+    }
+
+    public List<Course> viewCourses(){
+        return null;
+    }
+
+    public List<Course> viewRegisteredCourses(){
+        return null;
+    }
+
+    public double calculateFee(){
+        return 0.0;
+    }
+}
