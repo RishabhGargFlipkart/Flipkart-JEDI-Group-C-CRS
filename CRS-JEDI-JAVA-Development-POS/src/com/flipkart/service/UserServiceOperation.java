172caa5aecb7a63bf8cf5e5ded383c66a2e01ee2
NidhiManek <nidhi.b@BLRETV-G6LD2G0.local>
2023-01-31 10:53:44 +0530

    UserServiceOperation added

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/service/UserServiceOperation.java
@@ -0,0 +1,16 @@
+package com.flipkart.service;
+
+public class UserServiceOperation {
+
+    public String getRole(String userId) {
+
+    }
+
+    public boolean changePassword(String userID, String password) {
+
+    }
+
+    public boolean verifyCredentials(String userID, String password) {
+
+    }
+}
