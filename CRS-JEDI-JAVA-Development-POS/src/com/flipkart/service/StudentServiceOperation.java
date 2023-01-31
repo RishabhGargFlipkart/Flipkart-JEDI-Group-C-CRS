f3f2519b098456b4c5032b20e5f00d791a489665
RishabhGargFlipkart <123539417+RishabhGargFlipkart@users.noreply.github.com>
2023-01-31 11:10:41 +0530

    StudentServiceOperation

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/service/StudentServiceOperation.java
@@ -0,0 +1,13 @@
+package src.com.flipkart.service;
+
+public class StudentServiceOperation {
+    public int register(String name, String userId, String password, String branch, int batch) {
+
+    }
+    public int getStudentId(String userId) {
+
+    }
+    public boolean isApproved(int studentId) {
+
+    }
+}
