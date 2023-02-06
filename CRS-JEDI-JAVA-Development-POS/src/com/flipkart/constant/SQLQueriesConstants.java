a4153edb24c346f0dad0be636d6d0d77a9f50fa9
RishabhGargFlipkart <123539417+RishabhGargFlipkart@users.noreply.github.com>
2023-02-06 15:21:02 +0530

    sql_query_updates

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/constant/SQLQueriesConstants.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/constant/SQLQueriesConstants.java
@@ -5,7 +5,7 @@ public class SQLQueriesConstants {
     public static final String ADD_COURSE_QUERY = "insert into Course(courseCode, courseName, catalogId) values (?, ?, ?)";
     public static final String VIEW_PENDING_ADMISSION_QUERY = "select userId, name, password, role, gender, address, country, studentId from student natural join user where isApproved = 0";
     public static final String APPROVE_STUDENT_QUERY = "update Student set isApproved = 1 where studentId = ?";
-    public static final String ADD_USER_QUERY = "insert into User(userid, name, role, password) values (?, ?, ?, ?)";
+    public static final String ADD_USER_QUERY = "insert into User(userid, name, role, password, gender, address) values (?, ?, ?, ?, ?, ?)";
     public static final String ADD_PROFESSOR_QUERY = "insert into Professor(userId, department, designation) values (?, ?, ?)";
     public static final String ASSIGN_COURSE_QUERY = "update Course set professorId = ? where courseCode = ?";
     public static final String VIEW_COURSE_QUERY = "select courseCode, courseName, professorId from Course where catalogId = ?";
