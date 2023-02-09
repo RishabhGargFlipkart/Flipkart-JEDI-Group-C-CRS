9d6931adb0e6b507f77776db910973322abcea97
RishabhGargFlipkart <123539417+RishabhGargFlipkart@users.noreply.github.com>
2023-02-09 16:41:00 +0530

    Delete dummy directory

--- dummy/src/main/java/com/flipkart/constant/ColourConstant.java
+++
@@ -1,7 +0,0 @@
-package com.flipkart.constant;
-
-public class ColourConstant {
-    public static String ANSI_YELLOW = "\u001B[33m";
-    public static String ANSI_RESET = "\u001B[0m";
-    public static String ANSI_BLUE = "\u001B[34m";
-}
